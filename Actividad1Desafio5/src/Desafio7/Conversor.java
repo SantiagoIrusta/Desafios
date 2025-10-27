d2a1a3c1efb9e30066466367a7be4a95f74b6926
SantiagoIrusta <santiagoirusta4238@gmail.com>
2025-10-27 20:50:41 -0300

    ADD: Desafio 7 completo

---
+++ Actividad1Desafio5/src/Desafio7/Conversor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Desafio7;
+
+/**
+ *
+ * @author santi
+ */
+public class Conversor {
+    
+    public static double celsiusAFahrenheit(double c){
+        return (c * 9/5) + 32;
+    }
+    
+    public static double fahrenheitACelsius(double f){
+        return (f - 32) * 5/9;
+    }
+    
+}
